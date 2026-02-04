06ac9d8ca4c35ed03bb404d5daf7e9b2dd0e8331
jwit11 <jwitnetlight@icloud.com>
2026-02-04 13:27:19 +0100

    Update timer duration and improve display logic

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let time = 1500;
+// Timer starten: 25 Minuten
+let time = 25 * 60;
+let timerEl = document.querySelector("h1");
 
 setInterval(() => {
   let minutes = Math.floor(time / 60);
   let seconds = time % 60;
-  document.querySelector("h1").innerText =
-    minutes + ":" + seconds.toString().padStart(2, "0");
+  timerEl.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
   time--;
 }, 1000);
 
