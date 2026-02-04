f0491be9f539f82e36768300c675d1e40a19e305
jwit11 <jwitnetlight@icloud.com>
2026-02-04 13:42:39 +0100

    Update console log message in script.js

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
-console.log("Timer Script geladen"); 
-// --- Pomodoro Timer Logik ---
+console.log("JS funktioniert!");
 
-let focusTime = 25 * 60; // 25 Minuten
-let breakTime = 5 * 60;  // 5 Minuten
-let time = focusTime;
-
-let isRunning = false;
-let isFocus = true;
-let timerInterval = null;
-
-// Elemente aus HTML
-const timerEl = document.querySelector("h1");
-const stateEl = document.querySelector("p");
-
-// Timer Update Funktion
-function updateTimerDisplay() {
-  let minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-  timerEl.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
-  stateEl.textContent = i
